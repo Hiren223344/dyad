bfae38e211b8350f2bfa52cb02c1c421949bdec9
Hiren <ghg64272@gmail.com>
2026-01-04 15:25:45 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
+  test: {
+    environment: "happy-dom",
+    globals: true,
   },
 });
